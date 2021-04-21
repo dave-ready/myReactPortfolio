818774142aca1c9efd710ff7296b2b1ae1ec4aaf
David Ready <davis_ready@hotmail.com>
2021-04-21 18:05:23 -0400

    imported more src files + updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom"; 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import "./App.css";
+import Home from './Components/Index';
+import Project from './Components/Project';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <>
-    <h2>App component</h2>
-    </>
+  <CssBaseline />
+  <Route exact path="/" component={Home} />
+  <Route path="/project" component={Project} />
+  <Route path="/contact" component={Contact} />
+   </> 
   );
 }
 
 export default App;
+
+
+   //
+   //
